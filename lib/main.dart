7ee660a027b6deb86119751e0b95f171ecdd9255
Fernando Batista <fernando.batista@ebanx.com>
2021-09-16 14:16:24 -0300

    Implemented nuvigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_playground/app_inject.dart';
 import 'package:flutter_playground/modules/home/presentation/home_page.dart';
+import 'package:flutter_playground/modules/qrcode/presentation/qrcode_page.dart';
+import 'package:nuvigator/next.dart';
 import 'package:stark/stark.dart';
 
 void main() {
@@ -17,7 +19,24 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      builder: Nuvigator(
+        router: AppRouter(),
+      ),
     );
   }
+}
+
+class AppRouter extends NuRouter {
+  @override
+  String get initialRoute => 'home';
+
+  @override
+  List<NuRoute<NuRouter, Object, Object>> get registerRoutes => [
+        NuRouteBuilder(
+          path: 'home',
+          builder: (_, __, ___) => HomePage(),
+          screenType: MaterialScreenType(),
+        ),
+        NuRouteBuilder(path: 'qrcode', builder: (_, __, ___) => QrCodePage()),
+      ];
 }
--- lib/modules/home/presentation/home_page.dart
+++ lib/modules/home/presentation/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_playground/modules/home/domain/usecases/get_features_usecase.dart';
 import 'package:flutter_playground/modules/home/presentation/home_cubit.dart';
 import 'package:flutter_playground/modules/home/presentation/home_state.dart';
 import 'package:flutter_playground/modules/qrcode/presentation/qrcode_page.dart';
